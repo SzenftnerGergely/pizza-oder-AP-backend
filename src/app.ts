063291e26ec13421a240130ee11cec433e25f868
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-08-16 09:02:37 +0200

    express statis added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import fs from "fs/promises"
 import type { Request, Response } from "express"
+import cors from "cors"
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+app.use(express.static('data'))
 
 const port = 3000
 
